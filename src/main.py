2c4522acaa1f388045e270f2daa9bddb574a83ad
Momina Antonina <135429329+userownmaa@users.noreply.github.com>
2025-05-21 17:24:03 +0300

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,84 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+filepath = None
+
+
+def open_file(window, text_edit):
+    global filepath
+    filepath = askopenfilename(filetypes=[("Text Files", "*.txt")])
+
+    if not filepath:
+        return
+
+    text_edit.delete(1.0, tk.END)
+    with open(filepath, "r") as f:
+        content = f.read()
+        text_edit.insert(tk.END, content)
+    window.title(f"Open File: {filepath}")
+
+
+def save_file(window, text_edit):
+    global filepath
+
+    if not filepath:
+        save_as_file(window, text_edit)
+        return
+
+    with open(filepath, "w") as f:
+        content = text_edit.get(1.0, tk.END)
+        f.write(content)
+    window.title(f"Open File: {filepath}")
+
+
+def save_as_file(window, text_edit):
+    global filepath
+    filepath = asksaveasfilename(filetypes=[("Text Files", "*.txt")])
+
+    if not filepath:
+        return
+
+    with open(filepath, "w") as f:
+        content = text_edit.get(1.0, tk.END)
+        f.write(content)
+    window.title(f"Open File: {filepath}")
+
+
+def new_file(window, text_edit):
+    global filepath
+    filepath = None
+
+    text_edit.delete(1.0, tk.END)
+    window.title("Open File: Untitled")
+
+
+def main():
+    window = tk.Tk()
+    window.title("Text Editor")
+    window.rowconfigure(0, minsize=400)
+    window.columnconfigure(1, minsize=500)
+
+    text_edit = tk.Text(window, font="Helvetica 18")
+    text_edit.grid(row=0, column=1)
+
+    frame = tk.Frame(window, relief=tk.RAISED, bd=2)
+    save_button = tk.Button(frame, text="Save", command=lambda: save_file(window, text_edit))
+    save_as_button = tk.Button(frame, text="Save As", command=lambda: save_as_file(window, text_edit))
+    open_button = tk.Button(frame, text="Open", command=lambda: open_file(window, text_edit))
+    new_button = tk.Button(frame, text="New", command=lambda: new_file(window, text_edit))
+
+    save_button.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+    save_as_button.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
+    open_button.grid(row=2, column=0, padx=5, pady=5, sticky="ew")
+    new_button.grid(row=3, column=0, padx=5, pady=5, sticky="ew")
+    frame.grid(row=0, column=0, sticky="ns")
+
+    window.bind("<Control-s>", lambda x: save_file(window, text_edit))
+    window.bind("<Control-Shift-S>", lambda x: save_as_file(window, text_edit))
+    window.bind("<Control-o>", lambda x: open_file(window, text_edit))
+    window.bind("<Control-n>", lambda x: new_file(window, text_edit))
+
+    window.mainloop()
+
+
+main()
